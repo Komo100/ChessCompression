7937d7298f8d964eb97a31126c89a224463cd8c4
KomoBIG <cubeque@wp.pl>
2023-06-29 21:52:45 +0200

    Working Huffman Encoding

--- main.cpp
+++ main.cpp
@@ -1,5 +1,3 @@
-#define N 5
-
 //#include "Huffman.h"
 #include <string>
 #include <map>
@@ -9,65 +7,106 @@
 #include <iostream>
 
 struct node {
-    std::bitset<1> code;
-    char symbol[N];
+    char code;
+    std::string symbol;
     int frequency;
     node* left;
     node* right;
+
+    node(std::string symboll, int frequencyy) {
+        code = '0';
+        symbol = symboll;
+        frequency = frequencyy;
+        left = NULL;
+        right = NULL;
+    }
+    node() {};
 };
-
-void copy_chars(const char in[N], char out[N]) {
-    for(int i = 0; i < N; i++) out[i] = in[i];
-}
-
-struct node* NewNode(char symbol[5], std::bitset<1> code, int frequency) {
-    struct node* node
-        = (struct node*)malloc(sizeof(struct node));
- 
-    // Assign data to this node
-    node->code = code;
-    copy_chars(symbol, node->symbol);
-    node->frequency = frequency;
-    // Initialize left and
-    // right children as NULL
-    node->left = NULL;
-    node->right = NULL;
-    return (node);
-}
 
 
 int main() {
 
-    std::map<char[N], int> Symbols;
-    std::map<char[N], int>::iterator it;
+    std::map<std::string, int> Symbols;
+    std::map<std::string, int>::iterator it;
 
-    Symbols["A000"] = 1;
-    Symbols["B000"] = 4;
-    Symbols["C000"] = 3;
-    Symbols["D000"] = 2;
-    std::vector<node*> Nodes;
-    std::vector<node*> Queue;
+    Symbols["C"] = 2;
+    Symbols["B"] = 6;
+    Symbols["E"] = 7;
+    Symbols["_"] = 10;
+    Symbols["D"] = 10;
+    Symbols["A"] = 11;
+
+    std::vector<node> Queue;
     
-    std::map<char[N], int> Buff = Symbols;
+    std::map<std::string, int> Buff = Symbols;
     int frequency; 
-    char symbol[5] = "1111";
+    std::string symbol;
     for(int i = 0; i < Symbols.size(); i++) {
         frequency = 0;
         for(it = Buff.begin(); it != Buff.end(); it++) {
             std::cout << "siema" << std::endl;
             if(it->second > frequency) {
-                copy_chars(it->first, symbol);
+                symbol = it->first;
                 frequency = it->second;
             }
         }
-        Queue.push_back(NewNode(symbol, 0b0, frequency));
+        node a = node(symbol, frequency);
+        Queue.push_back(a);
         Buff.erase(symbol);
     }
     reverse(Queue.begin(), Queue.end());
-
     for(int i = 0; i < Queue.size(); i++) {
-        std::cout << Queue.at(i)->symbol << Queue.at(i)->code << " " << Queue.at(i)->frequency << std::endl;
+        std::cout << Queue.at(i).symbol << " " << Queue.at(i).code << " " << Queue.at(i).frequency << std::endl;
     }
-
+    int size2 = Queue.size()*Queue.size();
+    node* Nodes = new node[size2]; 
+    int Nodes_counter = 0;
+    while(!Queue.empty()) {
+        Queue.at(0).code = '0';
+        Queue.at(1).code = '1';
+        node a = node(Queue.at(0).symbol + Queue.at(1).symbol, Queue.at(0).frequency + Queue.at(1).frequency);
+        Nodes[Nodes_counter] = Queue.at(0);
+        a.left = &Nodes[Nodes_counter];
+        Nodes_counter ++;
+        Nodes[Nodes_counter] = Queue.at(1);
+        a.right = &Nodes[Nodes_counter];
+        Nodes_counter ++;
+        Queue.erase(Queue.begin(), Queue.begin() + 2);
+        for(int i = 0; i < Queue.size(); i++) {
+            //std::cout << Queue.at(i).symbol << std::endl;
+            if(a.frequency < Queue.at(i).frequency) {
+                Queue.insert(Queue.begin() + i, a);
+                break;
+            }
+            if(i == Queue.size() - 1) {
+                Queue.push_back(a);
+                break;
+            }
+        }
+        if(Queue.size() == 0) {
+            Nodes[Nodes_counter] = a;
+            Nodes_counter++;
+            break;
+        }
+    }
+    for(it = Symbols.begin(); it != Symbols.end(); it++) {
+        std::cout << it->first << " ";
+        node a = Nodes[Nodes_counter - 1];
+        std::string code = "";
+        int i = 0;
+        while(a.left != NULL) {
+            if(a.left->symbol.find(it->first) != std::string::npos) {
+                code += a.left->code;
+                a = *a.left;                
+            }
+            else {
+                code += a.right->code;
+                a = *a.right;
+            }
+            i++;
+        }
+        std::cout << code << std::endl; 
+    }
+    delete[] Nodes;
     return 0;
 }
